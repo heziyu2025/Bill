016e2ce7c069ca3825ec6e1c80ed254983d7f05f
Tom H <ziyu2025@gmail.com>
2024-08-06 12:27:02 +0800

    增加切换页面时的记忆功能

--- chart_view.py
+++ chart_view.py
@@ -1,5 +1,10 @@
+from typing import List
 import flet as ft
 
+from transaction import Transaction
 
-def chart_view(page: ft.Page):
+
+def chart_view(page: ft.Page,
+               transactions: List[Transaction],
+               transaction_list_tiles: List[ft.ListTile]):
     return ft.Text('aaa')
---
+++ transaction.py
@@ -0,0 +1,30 @@
+import datetime
+
+import flet as ft
+
+
+class Transaction:
+    def __init__(self, is_income: bool, type_: str, amount: float, create_time: datetime):
+        self.is_income = is_income
+        self.type_ = type_
+        self.amount = amount
+        self.create_time = create_time
+
+    def to_list_tile(self):
+        icon_type = ft.icons.ARROW_UPWARD if self.is_income else ft.icons.ARROW_DOWNWARD
+        return ft.ListTile(
+            leading=ft.Icon(icon_type),
+            title=ft.Text(str(self.amount), expand=True),
+            subtitle=ft.Text(self.type_, expand=True),
+            trailing=ft.Row(
+                [
+                    ft.Text(self.create_time.strftime('%Y-%m-%d %H:%M:%S')),
+                    ft.PopupMenuButton(
+                        items=[
+                            ft.PopupMenuItem('Delete', icon=ft.icons.DELETE)
+                        ]
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.END
+            )
+        )
