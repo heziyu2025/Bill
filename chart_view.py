a33fd6028b6bc9de4f25ffaa0a090fd154fbe107
Evan <hyh618@gmail.com>
2024-08-11 14:09:20 +0800

    Update chart_view.py
    data visualizing

--- chart_view.py
+++ chart_view.py
@@ -10,30 +10,34 @@ from flet.matplotlib_chart import MatplotlibChart
 def chart_view(page: ft.Page,
                transactions: List[Transaction],
                transaction_list_tiles: List[ft.ListTile]):
-    # Fixing random state for reproducibility
-    np.random.seed(19680801)
-
-    dt = 0.01
-    t = np.arange(0, 30, dt)
-    nse1 = np.random.randn(len(t))  # white noise 1
-    nse2 = np.random.randn(len(t))  # white noise 2
-
-    # Two signals with a coherent part at 10Hz and a random part
-    s1 = np.sin(2 * np.pi * 10 * t) + nse1
-    s2 = np.sin(2 * np.pi * 10 * t) + nse2
 
     fig, axs = plt.subplots(2, 1)
-    axs[0].plot(t, s1, t, s2)
-    axs[0].set_xlim(0, 2)
     axs[0].set_xlabel("time")
-    axs[0].set_ylabel("s1 and s2")
+    axs[0].set_ylabel("Income")
+    axs[1].set_ylabel("Consumption")
+
+    # 分别提取收入和支出的 amounts 和 create_time
+    income_transactions = [(transaction.amount, transaction.create_time.date())
+                           for transaction in transactions if transaction.is_income]
+    print(income_transactions)
+
+    expense_transactions = [(transaction.amount, transaction.create_time.date())
+                            for transaction in transactions if not transaction.is_income]
+    print(expense_transactions)
+
+    fig, axs = plt.subplots(2, 1)
+    axs[0].plot(income_transactions)
+    axs[0].set_xlabel("time")
+    axs[0].set_ylabel("Income")
     axs[0].grid(True)
 
-    cxy, f = axs[1].cohere(s1, s2, 256, 1.0 / dt)
-    axs[1].set_ylabel("coherence")
+    axs[1].plot(expense_transactions)
+    axs[1].set_xlabel("time")
+    axs[1].set_ylabel("Consumption")
+    axs[1].grid(True)
 
     fig.tight_layout()
 
     page.add(MatplotlibChart(fig, expand=True))
 
-    return ft.Text('aaa')
+    return ft.Text('1月~6月')
